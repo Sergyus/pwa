6d7b455295deffac19319db4604097149cadbc01
Serj Mes <mes28293@gmail.com>
2021-01-17 22:55:39 +0200

    Mes

--- src/pages/Pwa.tsx
+++ src/pages/Pwa.tsx
@@ -1,6 +1,7 @@
 import Title from '../components/Title';
 import pic from './../assets/img/pwa.jpg';
 import pic2 from './../assets/img/pwa2.jpg';
+import pic3 from './../assets/img/pwa3.jpg';
 
 export default function Pwa(): JSX.Element {
   return (
@@ -10,6 +11,9 @@ export default function Pwa(): JSX.Element {
       <br />
       <br />
       <img style={{ width: '100%' }} src={pic2} alt="pic" />
+      <br />
+      <br />
+      <img style={{ width: '100%' }} src={pic3} alt="pic" />
     </>
   );
 }
